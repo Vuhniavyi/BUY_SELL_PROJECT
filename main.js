e5f9fe170dbcfd0e592a29abbb55655dcc5159a0
a1286 <a1286@MacBook-Pro-a1286.local>
2019-08-24 12:23:34 +0300

    End of project

---
+++ main.js
@@ -0,0 +1,9 @@
+//falling out modal1
+$(document).ready(function() {
+  $('.do-open').on('click', function() {
+      $('#modal').css('display', 'block');
+  });
+  $('.do-close').on('click', function() {
+      $('#modal').css('display', 'none');
+  });
+});
